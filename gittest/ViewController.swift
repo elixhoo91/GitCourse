db5e36ffd65a27b48e046589fefcc5e46027a78d
elixhoo91 <aptsyssupport@aptsyss-macbook-air.local>
2016-06-18 21:52:56 +0800

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Aptsys Support on 18/6/16.
+//  Copyright © 2016 Aptsys Support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
